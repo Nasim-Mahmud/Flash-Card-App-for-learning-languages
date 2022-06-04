4525e887b94df97af7329c70c65cdc6ae912d11f
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-06-04 15:50:47 +0600

    Adding dependencies.

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+from tkinter import *
+
+
+
+
+
+#--------------------------------------UI----------------------------------
 BACKGROUND_COLOR = "#B1DDC6"
+
+
+window = Tk()
+window.title("Flash Card")
+window.minsize(width=800, height=600)
+
+
+
+
+window.mainloop()
 
