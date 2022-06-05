88cb5682683441022af51e2970862752eb5f9ba0
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-06-05 21:37:01 +0600

    Adding Canvas.

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ window.title("Flash Card")
 window.config(bg=BACKGROUND_COLOR)
 window.minsize(width=800, height=600)
 
-canvas = Canvas(height=526, width=800, highlightthickness=0)
+canvas = Canvas(height=526, width=800, highlightthickness=0, bg=BACKGROUND_COLOR)
 card_image = PhotoImage(file="./images/card_front.png")
 canvas.create_image(400, 263, image=card_image)
-canvas.grid(row=0, column=0, pady=20)
+canvas.grid(row=0, column=0, pady=40, padx=40)
 
 window.mainloop()
