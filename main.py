fe68560a76223f5df36e7bedcea9d7d1ca8ffddc
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-06-05 21:56:16 +0600

    Adding Canvas.

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ card_image = PhotoImage(file="./images/card_front.png")
 canvas.create_image(400, 263, image=card_image)
 canvas.grid(row=0, column=1, pady=40)
 
-button_image_1 = PhotoImage(file="./images/wrong.png")
-button = Button(image=button_image_1, highlightthickness=0)
-button.grid(row=1, column=0)
-
-button_image_2 = PhotoImage(file="./images/right.png")
-button = Button(image=button_image_2, highlightthickness=0)
-button.grid(row=1, column=1)
+# button_image_1 = PhotoImage(file="./images/wrong.png")
+# button = Button(image=button_image_1, highlightthickness=0)
+# button.grid(row=1, column=0)
+#
+# button_image_2 = PhotoImage(file="./images/right.png")
+# button = Button(image=button_image_2, highlightthickness=0)
+# button.grid(row=1, column=1)
 
 window.mainloop()
