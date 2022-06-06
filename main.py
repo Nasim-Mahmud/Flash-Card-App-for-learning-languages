d8433c996c76e1a689ccd4b3e9c9e1cb04fbb344
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-06-06 14:18:18 +0600

    Adding labels.

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ button = Button(image=button_image_2, highlightthickness=0)
 button.grid(row=1, column=2, padx=50)
 
 label_language = Canvas(height=400, width=600, background=WHITE_BACKGROUND, highlightthickness=0)
-label_language.create_text(300, 103,text="Name", font=LANG_FONT, )
-label_language.create_text(300, 203,text="Name", font=WORD_FONT)
+label_language.create_text(320, 70,text="Name", font=LANG_FONT, )
+label_language.create_text(320, 200,text="Name", font=WORD_FONT)
 label_language.grid(row=0, column=1, columnspan=3)
 
 # label_word = Canvas()
