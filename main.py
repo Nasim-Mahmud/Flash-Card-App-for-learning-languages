e8fbb5c2b7629690a36dbbc01ef8c8018bc859a1
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-06-06 14:06:49 +0600

    Adding labels.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from tkinter import *
 
 BACKGROUND_COLOR = "#B1DDC6"
-
+WHITE_BACKGROUND = "#FFFFFF"
+FONT = ("Calibri", 25, "normal")
 # --------------------------------------UI----------------------------------
 
 
@@ -22,5 +23,9 @@ button.grid(row=1, column=1, padx=150)
 button_image_2 = PhotoImage(file="./images/right.png")
 button = Button(image=button_image_2, highlightthickness=0)
 button.grid(row=1, column=2, padx=50)
+
+label = Label()
+label.config(text="Name", font=FONT, bg= WHITE_BACKGROUND)
+label.grid(row=0, column=1, columnspan=3)
 
 window.mainloop()
