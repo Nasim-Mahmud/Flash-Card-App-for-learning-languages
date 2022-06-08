56ce6bcbf4bd1a7fde54d6f7f7f719715fb1b5c0
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-06-08 13:03:02 +0600

    Adding labels.

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ WORD_FONT = ("Ariel", 50, "bold")
 
 window = Tk()
 window.title("Flash Card")
-window.config(bg=BACKGROUND_COLOR)
-window.minsize(width=600, height=700)
+window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 
 canvas = Canvas(height=526, width=800, highlightthickness=0, bg=BACKGROUND_COLOR)
 card_image = PhotoImage(file="./images/card_front.png")
