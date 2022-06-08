73f4e12618e5a58c971059d52bb7d3ec9b712d2d
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-06-08 13:04:52 +0600

    Fixing dependencies.

--- main.py
+++ main.py
@@ -15,17 +15,17 @@ window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 canvas = Canvas(height=526, width=800, highlightthickness=0, bg=BACKGROUND_COLOR)
 card_image = PhotoImage(file="./images/card_front.png")
 canvas.create_image(400, 263, image=card_image)
-canvas.create_text(300, 70,text="Title", font=LANG_FONT, )
-canvas.create_text(300, 200,text="Name", font=WORD_FONT)
+canvas.create_text(400, 150,text="Title", font=LANG_FONT, )
+canvas.create_text(400, 263,text="Name", font=WORD_FONT)
 canvas.grid(row=0, column=0)
 
 button_image_1 = PhotoImage(file="./images/wrong.png")
 button = Button(image=button_image_1, highlightthickness=0)
-button.grid(row=1, column=1, padx=150)
+button.grid(row=1, column=0)
 
 button_image_2 = PhotoImage(file="./images/right.png")
 button = Button(image=button_image_2, highlightthickness=0)
-button.grid(row=1, column=2, padx=50)
+button.grid(row=1, column=1, padx=50)
 
 
 window.mainloop()
