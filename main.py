044ce06c0b51baf1d8a267b549f90e2e9e4691a5
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-06-08 14:12:45 +0600

    Reading data from csv.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from tkinter import *
 import random
 import pandas
 
-
 BACKGROUND_COLOR = "#B1DDC6"
 WHITE_BACKGROUND = "#FFFFFF"
 LANG_FONT = ("Ariel", 30, "italic")
@@ -10,9 +9,13 @@ WORD_FONT = ("Ariel", 50, "bold")
 
 
 # --------------------------------ACCESSING DATASET------------------------
+data = pandas.read_csv("./data/french_words.csv")
+
 def random_word():
-    data = pandas.read_csv("./data/french_words.csv")
-    return random.choice(data.French)
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=random.choice(data.French))
+
+
 # random_word()
 # --------------------------------------UI----------------------------------
 
@@ -23,16 +26,16 @@ window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 canvas = Canvas(height=526, width=800, highlightthickness=0, bg=BACKGROUND_COLOR)
 card_image = PhotoImage(file="./images/card_front.png")
 canvas.create_image(400, 263, image=card_image)
-canvas.create_text(400, 150, text="Title", font=LANG_FONT)
-canvas.create_text(400, 263, text=random_word(), font=WORD_FONT)
+card_title = canvas.create_text(400, 150, text="Title", font=LANG_FONT)
+card_word = canvas.create_text(400, 263, text="Name", font=WORD_FONT)
 canvas.grid(row=0, column=0, columnspan=2)
 
 button_image_1 = PhotoImage(file="./images/wrong.png")
-button = Button(image=button_image_1, highlightthickness=0, command=random_word)
-button.grid(row=1, column=0)
+wrong_button = Button(image=button_image_1, highlightthickness=0, command=random_word)
+wrong_button.grid(row=1, column=0)
 
 button_image_2 = PhotoImage(file="./images/right.png")
-button = Button(image=button_image_2, highlightthickness=0, command=random_word)
-button.grid(row=1, column=1, padx=50)
+right_button = Button(image=button_image_2, highlightthickness=0, command=random_word)
+right_button.grid(row=1, column=1, padx=50)
 
 window.mainloop()
